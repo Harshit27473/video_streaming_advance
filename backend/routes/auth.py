66348246ea1d9e93aef3635aba8d2f21d7d9236e
Harshit27473 <harshitduhan2004@gmail.com>
2026-03-31 16:36:00 +0530

    get user data

--- backend/routes/auth.py
+++ backend/routes/auth.py
@@ -1,10 +1,10 @@
-
+from db.middleware.auth_middleware import get_current_user
 from db.models.user import User
 from db.db import get_db
 from helper.auth_helper import get_secret_hash
 from secret_keys import SecretKeys
 from pydantic_models.auth_models import LoginRequest, SignupRequest, confirmSignupRequest
-from fastapi import APIRouter, Depends, HTTPException, Response
+from fastapi import APIRouter, Depends, HTTPException, Response , Cookie
 import boto3
 from sqlalchemy.orm import Session
 
@@ -118,4 +118,53 @@ def confirm_signup(
         )
         return {"message": "user confirmed successfully"}
     except Exception as e:
+        raise HTTPException(400, f'cognito confirm signup exception {e}')
+
+
+@router.post("/refresh")
+def refresh_token(
+    refresh_token: str = Cookie(None), 
+    user_cognito_sub: str = Cookie(None),
+    response: Response = None
+    ):
+    try:
+        secret_hash = get_secret_hash(user_cognito_sub, COGNITO_CLIENT_ID, COGNITO_SECRET_KEY)
+
+        cognito_response =cognito_client.initiate_auth(
+            ClientId=COGNITO_CLIENT_ID,
+            AuthFlow="REFRESH_TOKEN_AUTH",
+            AuthParameters={
+                'REFRESH_TOKEN': refresh_token,
+                'SECRET_HASH': secret_hash,
+            } 
+        )
+        auth_result = cognito_response.get("AuthenticationResult")
+
+        if not auth_result:
+            raise HTTPException(400, 'cognito did not return authentication result')
+
+        access_token = auth_result.get("AccessToken")
+        refresh_token = auth_result.get("RefreshToken")
+
+        response.set_cookie(
+            key="access_token", 
+            value=access_token, 
+            httponly=True, 
+            secure= True
+            )
+        
+        response.set_cookie(
+            key="refresh_token", 
+            value=refresh_token, 
+            httponly=True, 
+            secure= True
+            )
+        
+        return {"message": "token refreshed successfully"}
+    except Exception as e:
         raise HTTPException(400, f'cognito login exception {e}')
+    
+
+@router.get("/me")
+def protected_route(user =Depends(get_current_user)):
+    return {'message': 'you are authenticated', 'user': user}
