70ed97b0dcc0f604dd9c96a0f05780db3b6a454a
Harshit27473 <harshitduhan2004@gmail.com>
2026-04-01 15:02:26 +0530

    video uploading in s3 bucket

--- consumer/secret_keys.py
+++ consumer/secret_keys.py
@@ -5,10 +5,8 @@ from pydantic import Field
 load_dotenv()  # Load environment variables from .env file
 
 class SecretKeys(BaseSettings):
-    COGNITO_CLIENT_ID: str = ""
-    COGNITO_CLIENT_SECRET: str = ""
     REGION_NAME: str = ""
-    POSTGRES_DB_URL: str = ""
+    AWS_SQS_VIDEO_PROCESSING: str = ""
     
 
     
