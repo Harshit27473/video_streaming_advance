d473822d1f8ee6c8b15b6cac5136af3a9f8f5562
Harshit27473 <harshitduhan2004@gmail.com>
2026-04-06 15:51:45 +0530

    flutter initial commit

---
+++ flutter_client/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_client/pages/auth/signup_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Video Streaming App',
+      theme: ThemeData(
+        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const SignupPage(),
+    );
+  }
+}
